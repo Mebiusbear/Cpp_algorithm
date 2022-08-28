5bee4b252a26f687062302fcd25c0988cbb29747
Mebiusbear <460546692@qq.com>
2022-08-29 00:05:06 +0800

    complete dir

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <first.hpp>
+
+
+using namespace std;
+
+int main(){
+
+    cout << "This is main!"
+         << endl;
+
+    one2ten();
+
+}
---
+++ src/first.cpp
@@ -0,0 +1,10 @@
+#include "first.hpp"
+#include <iostream>
+
+using namespace std;
+void one2ten()
+{
+    for (int i=0;i<10;i++){
+        cout << i << endl;
+        }
+}
